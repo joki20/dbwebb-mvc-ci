210f20e76f100c9f729287fce8e74ce6b3a81519
Johan Kristoffersson <joki20@student.bth.se>
2021-05-10 19:15:09 +0100

    project-setup override true i build för scrutinizer

--- routes/console.php
+++ routes/console.php
@@ -17,10 +17,3 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
-
-
-if ('production' === App::environment()) {
-    Artisan::command('migrate', function () {
-        $this->comment('You are not allowed to do this in production!');
-    })->describe('Override default command in production.');
-}
